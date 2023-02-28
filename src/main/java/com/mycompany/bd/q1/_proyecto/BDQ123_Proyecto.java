94dba0c7316ba492708a4734d44740daed9bbf54
mario <mario@DESKTOP-79MCB0S>
2023-02-27 22:27:41 -0600

    Base del proyecto

---
+++ src/main/java/com/mycompany/bd/q1/_proyecto/BDQ123_Proyecto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bd.q1._proyecto;
+
+/**
+ *
+ * @author mario
+ */
+public class BDQ123_Proyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
